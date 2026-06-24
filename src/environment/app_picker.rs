3d22ac15eb78bf53538fd96ba295b6dc914d4e55
NewtOfficial25 <jucicleidealves1966@gmail.com>
2026-06-24 12:54:03 -0300

    Update app_picker.rs

--- src/environment/app_picker.rs
+++ src/environment/app_picker.rs
@@ -297,7 +297,7 @@ fn show_app_picker_gui(
             "" => include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/res/icon.png")),
             "UNOFFICIAL" => include_bytes!(concat!(
                 env!("CARGO_MANIFEST_DIR"),
-                "/res/icon_newthle.png"
+                "/res/icon_unofficial.png"
             )),
             "PREVIEW" => {
                 include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/res/icon_preview.png"))
